56b220147542cad24668505219ee01699e7ed548
DinaSem <neispravimayas1@gmail.com>
2022-10-06 14:34:06 +0300

    adaptivity

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -5,7 +5,19 @@ export const Header = () => {
     return (
         <div className={s.header}>
             <div className={s.header_logo}>LoremIpsum.<span className={s.header_logo_blue}>Net</span></div>
-            <div className={s.header_menu}>
+            <div className={s.header_hamburger}>
+                <input className={s.menu_toggle} type="checkbox"/>
+                <label className={s.menu_button_container} htmlFor="menu-toggle">
+                    <div className={s.menu_button}></div>
+                </label>
+                <div className={s.header_menu}>
+                    <li className={s.header_menu_item}>Бизнес</li>
+                    <li className={s.header_menu_item}>О нас</li>
+                    <li className={s.header_menu_item}>Цены</li>
+                    <li className={s.header_menu_item}>Оформить заказ</li>
+                </div>
+            </div>
+            <div className={s.header_menuFull}>
                 <li className={s.header_menu_item}>Бизнес</li>
                 <li className={s.header_menu_item}>О нас</li>
                 <li className={s.header_menu_item}>Цены</li>
