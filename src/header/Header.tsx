238977a9337e24ff153ed2ec0d8c2a2b900b60c5
DinaSem <neispravimayas1@gmail.com>
2022-10-07 23:19:48 +0300

    hamburger

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,27 +3,21 @@ import s from './header.module.scss'
 
 export const Header = () => {
     return (
-        <div className={s.header}>
-            <div className={s.header_logo}>LoremIpsum.<span className={s.header_logo_blue}>Net</span></div>
-            <div className={s.header_hamburger}>
-                <input className={s.menu_toggle} type="checkbox"/>
-                <label className={s.menu_button_container} htmlFor="menu-toggle">
-                    <div className={s.menu_button}></div>
-                </label>
-                <div className={s.header_menu}>
-                    <li className={s.header_menu_item}>Бизнес</li>
-                    <li className={s.header_menu_item}>О нас</li>
-                    <li className={s.header_menu_item}>Цены</li>
-                    <li className={s.header_menu_item}>Оформить заказ</li>
-                </div>
+        <section className={s.header}>
+            <div className={s.header_logo}>
+                LoremIpsum.<span className={s.header_logo_blue}>Net</span>
             </div>
-            <div className={s.header_menuFull}>
+            <input id={s.header_menu_toggle} type="checkbox"/>
+            <label className={s.header_menu_button_container} htmlFor={s.header_menu_toggle}>
+                <div className={s.header_menu_button}></div>
+            </label>
+            <ul className={s.header_menu}>
                 <li className={s.header_menu_item}>Бизнес</li>
                 <li className={s.header_menu_item}>О нас</li>
                 <li className={s.header_menu_item}>Цены</li>
                 <li className={s.header_menu_item}>Оформить заказ</li>
-            </div>
-        </div>
+            </ul>
+        </section>
     );
 };
 
